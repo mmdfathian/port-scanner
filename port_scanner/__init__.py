f5f8fd871a466e024b7d3dad460c325de12b0474
Mohammad Fathian <167574608+mmdfathian@users.noreply.github.com>
2026-05-17 02:32:12 +0330

    Fix: correct author name from mmdfathin to mmdfathian

--- port_scanner/__init__.py
+++ port_scanner/__init__.py
@@ -1,7 +1,7 @@
 """Port Scanner - ابزار اسکن پورت‌های شبکه با پایتون"""
 
 __version__ = "1.0.0"
-__author__ = "mmdfathin"
+__author__ = "mmdfathian"
 
 from port_scanner.scanner import scan, scan_port
 from port_scanner.resolver import resolve_host
